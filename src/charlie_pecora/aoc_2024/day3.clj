ab1c65d8ba3fc7c3e3bacefcad01fa85c9d3e51a
Charlie Pecora <charlie.pecora@gmail.com>
2024-12-08 11:09:35 -0800

    day 3 part 2

--- src/charlie_pecora/aoc_2024/day3.clj
+++ src/charlie_pecora/aoc_2024/day3.clj
@@ -2,43 +2,47 @@
   (:require [clojure.java.io])
   (:require [clojure.string :as str]))
 
-(def sample-inputs (slurp "inputs/day3-sample.txt"))
+(def part1-sample-inputs (slurp "inputs/day3-sample.txt"))
+(def part2-sample-inputs "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 (def real-inputs (slurp "inputs/day3.txt"))
 
 (defn part1 [input]
-  (->> input 
+  (->> input
        (re-seq #"mul\((\d{1,3}),(\d{1,3})\)")
-        (map (fn [v] 
-               ;(
-                (* (Integer/parseInt (get v 1))(Integer/parseInt (get v 2)))
-                ;(def l (get v 1))
-                ;(def r (get v 2))
-                ;r
-               ;)
-             )
-          )
-        (reduce +)
-      )
- )
+       (map (fn [v]
+              (* (Integer/parseInt (get v 1)) (Integer/parseInt (get v 2)))))
 
+       (reduce +)))
 
 (defn part2 [input]
-  )
+  (def dont-splits (str/split input #"don't\(\)"))
+  (def do-blocks (str/join (concat
+                            (take 1 dont-splits)
+                            (map
+                             (fn [s] (get (str/split s #"do\(\)" 2) 1))
+                             (drop 1 dont-splits)))))
+
+  (->> do-blocks
+       (re-seq #"mul\((\d{1,3}),(\d{1,3})\)")
+       (map (fn [v]
+              (* (Integer/parseInt (get v 1)) (Integer/parseInt (get v 2)))))
+
+       (reduce +)))
+
 (defn main []
   (println "Day 3!")
 
   (println "Part 1!")
   (println "Sample run")
-  (println "  sample answer:" (part1 sample-inputs))
+  (println "  sample answer:" (part1 part1-sample-inputs))
   (println "Real run")
   (println "  real answer:" (part1 real-inputs))
 
   (println)
 
-  ;(println "Part 2!")
-  ;(println "Sample run")
-  ;(println "  sample answer:" (part2 sample-inputs))
-  ;(println "Real run")
-  ;(println "  real answer:" (part2 real-inputs))
-  )
+  (println "Part 2!")
+  (println "Sample run")
+  (println "  sample answer:" (part2 part2-sample-inputs))
+  (println "Real run")
+  (println "  real answer:" (part2 real-inputs)))
 
