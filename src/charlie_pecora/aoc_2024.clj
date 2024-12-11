db75a9765df61cf1ed4e31fc6cafb52e55c71a1d
Charlie Pecora <charlie.pecora@gmail.com>
2024-12-11 06:58:39 -0800

    day4 part2

--- src/charlie_pecora/aoc_2024.clj
+++ src/charlie_pecora/aoc_2024.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require [charlie-pecora.aoc-2024.day1 :as day1])
   (:require [charlie-pecora.aoc-2024.day2 :as day2])
-  (:require [charlie-pecora.aoc-2024.day3 :as day3]))
+  (:require [charlie-pecora.aoc-2024.day3 :as day3])
+  (:require [charlie-pecora.aoc-2024.day4 :as day4]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -12,5 +13,6 @@
     "day1" (day1/main)
     "day2" (day2/main)
     "day3" (day3/main)
+    "day4" (day4/main)
     (println day "not found")))
 
